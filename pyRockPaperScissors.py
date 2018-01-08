5dca31625f10989245167c7b0b95ba183cfd8a04
Lomar Lilly <lithiumlilly3@gmail.com>
2018-01-08 01:50:22 -0500

    Create pyRockPaperScissors.py

---
+++ pyRockPaperScissors.py
@@ -0,0 +1,24 @@
+import random
+moves = ["rock", "paper", "scissors"]
+
+def Computer():
+	"""
+  This function should simply return the computers SINGLE choice of rock, paper, or scissors IN LOWERCASE
+  """
+
+def Human():
+	"""
+  This function should display a menu, prompting the player to select either rock, paper, or scissors and return what the player 
+  selected IN LOWERCASE . If the player selects an invalid option, display the error message "Invalid Play"
+  """
+
+def main():
+	print("Welcome To PyRock Paper Scissors Best of 3\n")
+	"""
+  This function should prompt the player to enter his/her name, get the player's and computer's play, displays what the player and the 
+  computer played and displays who won. Remember this is a best of 3 match so this function should also keep track of who won each round 
+  and displays the final scores and the overall winner.
+  """
+	
+if __name__ == '__main__':
+	main()
